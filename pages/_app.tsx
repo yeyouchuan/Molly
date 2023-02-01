785a8dbd155eea5f13c9fa6fb83a9e585c8911e9
Chester How <chesterhow@gmail.com>
2023-02-01 15:26:01 +0800

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 
 import * as RadixTooltip from '@radix-ui/react-tooltip';
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 
 import { Layout } from '@/components/layouts/AppLayout';
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Analytics />
     </RadixTooltip.Provider>
   );
 }
